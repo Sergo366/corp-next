635b804afb3bec29313bc2142ca71efacd3cd090
skozenkov <skozenkov@deloitte.com>
2024-07-28 14:27:06 +0300

    Add popover to sing out

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,28 +7,60 @@ import {
   Input,
   Button,
   Avatar,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
 } from '@nextui-org/react';
 import { auth } from '@/auth';
+import {signIn, signOut} from "@/actions";
 
 export default async function Header() {
   const session = await auth();
 
   let authContent: React.ReactNode;
   if (session?.user) {
-    authContent = <Avatar src={session.user.image || ''} />;
+    authContent =
+        <Popover placement={'left'}>
+            <PopoverTrigger className={'cursor-pointer'}>
+                <Avatar src={session.user.image || ''} />
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className={'p-4'}>
+                    <form action={signOut}>
+                        <Button
+                            type={'submit'}
+                        >
+                            Sign Out
+                        </Button>
+                    </form>
+                </div>
+            </PopoverContent>
+        </Popover>
   } else {
     authContent = (
       <>
         <NavbarItem>
-          <Button type="submit" color="secondary" variant="bordered">
-            Sign In
-          </Button>
+            <form action={signIn}>
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="bordered"
+                >
+                    Sign In
+                </Button>
+            </form>
         </NavbarItem>
 
         <NavbarItem>
-          <Button type="submit" color="primary" variant="flat">
-            Sign Up
-          </Button>
+            <form action={signIn}>
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="flat"
+                >
+                    Sign Up
+                </Button>
+            </form>
         </NavbarItem>
       </>
     );
