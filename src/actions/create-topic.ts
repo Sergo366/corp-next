8ef136b236bc1a5a4cedd028cf2626e07a8ef8e9
skozenkov <skozenkov@deloitte.com>
2024-07-29 23:31:21 +0300

    Added validation, zod and form layout

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,45 @@
 'use server';
 
-export async function createTopic() {
-  // TODO: revalidate the homepage
+import {z} from "zod";
+import {auth} from "@/auth";
+
+const createTopicScheme = z.object({
+    name: z.string().min(3).regex(/[a-z-]/, {
+        message: 'Must be lowercase letters or dashes without spaces'
+    }),
+    description: z.string().min(10)
+})
+
+interface CreateTopicFormState {
+    errors: {
+        name?: string[];
+        description?: string[];
+        _form?: string[]
+    }
+}
+
+export async function createTopic(
+    formState: CreateTopicFormState,
+    formData: FormData
+): Promise<CreateTopicFormState> {
+    const result = createTopicScheme.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+    })
+
+    const session = await auth()
+
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ["You must be signed in to do this!"]
+            }
+        }
+    }
+
+    if (!result.success) {
+        return {errors: result.error.flatten().fieldErrors}
+    }
+
+    return {errors: {}}
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,9 +1,61 @@
+'use client'
+
+import {Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea} from "@nextui-org/react";
+import {createTopic} from "@/actions";
+import {useFormState} from 'react-dom'
 
 const TopicCreateForm = () => {
+
+    const [formState, action] = useFormState(createTopic, {
+        errors: {}
+    })
+
     return (
-        <div>
-            TopicCreateForm
-        </div>
+       <Popover placement={'left'}>
+            <PopoverTrigger>
+                <Button color={'primary'}>
+                    Create a Topic
+                </Button>
+            </PopoverTrigger>
+           <PopoverContent>
+               <form action={action}>
+                   <div className={'flex flex-col gap-4 p-4 w-80'}>
+                       <h3 className={'text-lg'}>
+                           Create a Topic
+                       </h3>
+                       <Input
+                           label={'Name'}
+                           labelPlacement={'outside'}
+                           placeholder={'Enter Name'}
+                           name={'name'}
+                           isInvalid={!!formState.errors.name}
+                           errorMessage={formState.errors.name?.join(', ')}
+                       />
+                       <Textarea
+                           name={'description'}
+                           label="Description"
+                           labelPlacement={'outside'}
+                           placeholder={'Describe your topic'}
+                           isInvalid={!!formState.errors.description}
+                           errorMessage={formState.errors.description?.join(', ')}
+                       />
+
+                       {formState.errors._form
+                           ? (
+                               <div className={'rounded p-2 bg-red-200 border border-red-400'}>
+                                   {formState.errors._form?.join(', ')}
+                               </div>
+                           )
+                           : null
+                       }
+
+                       <Button type="submit">
+                           Submit
+                       </Button>
+                   </div>
+               </form>
+           </PopoverContent>
+       </Popover>
     );
 };
 
